d79934b9506c60325eb51c6c323c41006507cbae
wangxu <wangxu102790@hotmail.com>
2017-02-27 15:32:05 +0800

    input

--- com/fine/android/finemonkeyrunner.py
+++ com/fine/android/finemonkeyrunner.py
@@ -43,7 +43,7 @@ class fineMonkeyRunner:
     #############################
     '''
 
-    def __init__(self, emulatorname,PHONE_EMULATOR_SWITCH):
+    def __init__(self, emulatorname,switch=PHONE_EMULATOR_SWITCH):
         device = self.waitforconnection(waitForConnectionTime, emulatorname)
         self.debug("__int__: creating the finemonkeyrunner object with emulatorname %s" % emulatorname)
         # use EasyMonkeyDevice and MonkeyDevice
@@ -53,6 +53,7 @@ class fineMonkeyRunner:
         self.DOWN = self.device.DOWN
         self.UP = self.device.UP
         self.DOWN_AND_UP = self.device.DOWN_AND_UP
+        self.switch = switch
         self.debug('created the fineEasyDevice')
 
     def debug(self, debuginfo):
@@ -70,6 +71,18 @@ class fineMonkeyRunner:
     def sleep(self, seconds):
         self.debug('sleeping %f seconds' % seconds)
         mr.sleep(seconds)
+
+    def installapp(self,apppath):
+        try:
+            self.device.installPackage(apppath)
+        except:
+            self.error("Install app Fail ")
+    def remove(self,packagename):
+        try:
+            self.device.removePackage(packagename)
+        except:
+            self.error("Uninstall app Fail ")
+
 
     '''建立device连接'''
     def waitforconnection(self, waitForConnectionTime,emulatorname):
@@ -110,7 +123,7 @@ class fineMonkeyRunner:
 
         except:
 
-            self.debug('isexist: the %dst time check id (%s) existing error ,  , will retry ' % (tmp, id))
+            #self.debug('isexist: the %dst time check id (%s) existing error ,  , will retry ' % (tmp, id))
             self.sleep(1)
 
         return False
@@ -142,20 +155,86 @@ class fineMonkeyRunner:
         self.debug('device input the %s' % content)
         self.device.type(content)
 
+    '''根据id点击元素'''
+    def clickbyid(self, id, type):
+
+        self.debug('calling clickbyid function')
+        for tmp in range(repeatTimesOnError):
+            try:
+                self.easydevice.touch(By.id(id), type)
+                return True
+            except:
+
+                self.debug(
+                    'clickbyid: the %dst time touch error by this id (%s) , not found the view , will retry ' % (
+                    tmp, id))
+                if (tmp > 1 & DEBUG):
+                    self.debug('Please wait to touch the view')
+                mr.sleep(1)
+                continue
+        self.error(
+            'clickbyid: sorry , still can\'t touch view. please check the view is exist or not , or increase the repeat times variable?')
+        sys.exc_info()
+        traceback.print_exc()
+        return False
+
+    '''判断给定的id元素是否是选中状态'''
+    def isfocused(self, id):
+
+        self.debug('checking the view is focused or not')
+        # hierarchyViewer = self.device.getHierarchyViewer()
+        # print hierarchyViewer.findViewById(id).hasFocus
+
+        for tmp in range(repeatTimesOnError):
+            try:
+                hierarchyViewer = self.device.getHierarchyViewer()
+                return hierarchyViewer.findViewById(id).hasFocus
+            except:
+                self.debug('isfocused: the %dst time check focus error  , will retry ' % tmp)
+                mr.sleep(1)
+                continue
+        self.error('isfocused: error occured')
+        sys.exc_info()
+        traceback.print_exc()
+        return False
+
+    '''获取指定的view文本'''
+    def gettextbyview(self, view):
+        if view != None:
+            return (view.namedProperties.get('text:mText').value.encode('utf8'))
+
+    '''清除编辑框的文本'''
     def cleartextbyid(self, id):
-        self.debug('calling clearTextById function by the id (%s)' % id)
-        if (self.checkIdExist(id)):
-            if not self.isFocused(id):
-                self.touchViewById(id, self.DOWN_AND_UP)
+        self.debug('calling cleartextbyid function by the id (%s)' % id)
+        if (self.isexist(id)):
+            if not self.isfocused(id):
+                self.clickbyid(id, self.DOWN_AND_UP)
             TextView = self.getview(id)
-            rangenumber = len(self.getText(TextView))
+            rangenumber = len(self.gettextbyview(TextView))
             for x in range(rangenumber):
                 self.device.press('KEYCODE_DEL', self.DOWN_AND_UP)
             for x in range(rangenumber):
                 self.device.press('KEYCODE_FORWARD_DEL', self.DOWN_AND_UP)
-            self.debug('clearTextById: cleared the text in id (%s)' % id)
+            self.debug('cleartextbyid: cleared the text in id (%s)' % id)
             return True
-        self.error('clearTextById: sorry ,the id (%s) is not exist ' % id)
+        self.error('cleartextbyid: sorry ,the id (%s) is not exist ' % id)
         sys.exc_info()
         traceback.print_exc()
         return False
+
+    '''根据id输入文本框内容'''
+    def inputtextbyid(self,id,text):
+
+        self.clickbyid(id,self.DOWN_AND_UP)
+        if self.isfocused(id):
+            self.type(text)
+        else:
+            self.debug('No Selected the element by id,Please check')
+
+        if self.switch =='P':
+            self.presskey('KEYCODE_BACK', self.DOWN_AND_UP, 1)
+
+        # 如果是真机需要打开语句，模拟器需关闭
+        # self.pressKey('KEYCODE_BACK', self.DOWN_AND_UP, 1)
+        # mr.sleep(1)
+        return True
